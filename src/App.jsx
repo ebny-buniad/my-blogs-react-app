4501ad8c777b8d11c8c14ff845fc867713148c49
ebnybuniad <ebnybuniad@hmail.com>
2025-04-06 11:06:13 +0600

    load blogs data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+
+      <Blogs></Blogs>
     </>
   )
 }
